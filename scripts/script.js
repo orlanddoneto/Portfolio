440c4d4070ce159d504517c173440bff8a30086b
Orlando Neto <neto020735@gmail.com>
2024-05-04 16:17:51 -0300

    Ajustes no footer, header e implementação do envio de email

--- scripts/script.js
+++ scripts/script.js
@@ -2,16 +2,25 @@
 const cabecalhoMenuContainer = document.querySelectorAll('.cabecalho__menu_container');
 
 // Seleciona o elemento #menu_sobre
-const menuSobre = document.querySelector('#page_atual');
+const menuAtual = document.querySelector('#page_atual');
 
 // Adiciona um ouvinte de evento para quando o mouse entra no elemento .cabecalho__menu_container
 cabecalhoMenuContainer.forEach(item=>{item.addEventListener('mouseenter', () => {
   // Altera a opacidade de #menu_sobre para 0
-  menuSobre.style.opacity = '0';})
+  menuAtual.style.opacity = '0';})
 
   item.addEventListener('mouseleave', () => {
     // Altera a opacidade de #menu_sobre para 0.25
-    menuSobre.style.opacity = '0.25';
+    menuAtual.style.opacity = '0.25';
   });
 });
 
+
+function toggleEmailPopup() {
+  var popup = document.getElementById("email-popup");
+  if (popup.style.display === "flex") {
+      popup.style.display = "none";
+  } else {
+      popup.style.display = "flex";
+  }
+}
